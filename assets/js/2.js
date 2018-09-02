590a89e0ad4ec76330d3f728dba636a519d87708
peraltawave <peraltawave@gmail.com>
2018-09-02 00:03:40 -0700

    third

--- assets/js/2.js
+++ assets/js/2.js
@@ -1,6 +1,6 @@
 var dogsArray = ["German Shepherd", "Anatolian Shepherd", "Border Collie"];
 
-$(document).ready(function() {
+$(document).ready(function () {
     for (var i = 0; i < dogsArray.length; i++) {
         $("#dog-buttons").append("<button type='button' onclick='searchGif(\"" + dogsArray[i] + "\")' class='btn btn-primary' value=' " + dogsArray[i] + "'> " + dogsArray[i] + " </button>");
     }
@@ -22,27 +22,52 @@ function submitButtonClicked() {
 
 function searchGif(imgName) {
     $.ajax({
-            url: 'https://api.giphy.com/v1/gifs/search?q= ' + imgName + ' &api_key=dc6zaTOxFJmzC&limit=9',
-            type: 'GET',
-        })
-        .done(function(response) {
+        url: 'https://api.giphy.com/v1/gifs/search?q= ' + imgName + ' &api_key=dc6zaTOxFJmzC&limit=9',
+        type: 'GET',
+    })
+        .done(function (response) {
             showGif(response);
         })
 }
 
+
+
+var playIcon = $('#clicker').on('click', function () {
+    alert("Hi")
+    
+    });
+
+    // $('#clicker').on('click', function () {
+    //     alert("Hi")}
+
+// var playIcon = $('.fas fa-play').on('click', function () {
+//     var state = $(this).attr('data-state');
+//     if (state == 'still') {
+//         $(this).attr('src', $(this).attr("data-animate"));
+//         $(this).attr('data-state', 'animate');
+//     } else {
+//         $(this).attr('src', $(this).attr("data-still"));
+//         $(this).attr('data-state', 'still');
+//     }
+
+//     });
+
+    console.log(playIcon);
+
 function showGif(response) {
+    var displayIcon = "<div id='clicker'><i class='fas fa-play'></i></div>";
     $('#images').empty();
-    for (var i = 0; i < response.data.length; i++) {
-        var rating = "<div class='ratings'> Rating:  " + (response.data[i].rating) + " </div>";
+    for (var i = 0; i < response.data.length; i++) { //this is the length of the data in the response object
+        var rating = "<div class='ratings'> Rated:  " + (response.data[i].rating) + " | " + displayIcon + "</div>";
         var image = rating + '<img src= " ' + response.data[i].images.fixed_height_still.url +
             '" data-still=" ' + response.data[i].images.fixed_height_still.url +
-            ' " data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:300px; height:300px; margin-bottom: 20px;">';
+            '" data-animate=" ' + response.data[i].images.fixed_height.url + '" data-state="still" class="movImage" style= "width:300px; height:300px; margin-bottom: 20px;">';
 
-        image = '<div class="col-md-4"><hr>' + image + "</div>";
+        image = '<div class="col-md-4">' + image + '<hr width="300"></div>';
         $('#images').append(image);
     }
 
-    $('.movImage').on('click', function() {
+    $('.movImage').on('click', function () {
         var state = $(this).attr('data-state');
         if (state == 'still') {
             $(this).attr('src', $(this).attr("data-animate"));
@@ -51,6 +76,9 @@ function showGif(response) {
             $(this).attr('src', $(this).attr("data-still"));
             $(this).attr('data-state', 'still');
         }
+        });
 
-    });
-}
+    }
+
+
+    
